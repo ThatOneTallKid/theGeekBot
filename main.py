c7dbb22550cab739c637d827010bc4cc955d47ad
adirups285 <55244625+adirups285@users.noreply.github.com>
2020-11-07 22:58:35 +0530

    'changes'

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+ID = os.getenv('DISCORD_ID')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+@client.event
+async def on_message(message):
+    id = client.get_guild(ID)
+    if message.content.find("!hello") != -1:
+        await message.channel.send("Hi") # If the user says !hello we will send back hi 
+    
 
+        
 client.run(TOKEN)
