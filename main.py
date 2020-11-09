bbe0275c999f035f0ecac48332ae6ac4efe3342a
adirups285 <55244625+adirups285@users.noreply.github.com>
2020-11-09 12:25:07 +0530

    'fcommands'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-
+import time
 import discord
+import asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,10 +20,26 @@ async def on_ready():
     )
 @client.event
 async def on_message(message):
-    id = client.get_guild(ID)
+    
+    bad_words = ["fuck", "stop", "45"]
     if message.content.find("!hello") != -1:
         await message.channel.send("Hi") # If the user says !hello we will send back hi 
     
+    for word in bad_words:
+        if message.content.count(word) > 0:
+            print("A bad word was said")
+            await message.channel.purge(limit=1)
 
+    if message.content == "!Help":
+        embed = discord.Embed(title="Help on BOT", description="Some user commands")
+        embed.add_field(name="!hello", value= "Greets the user")
+        await message.channel.send(content=None, embed=embed)        
+
+
+@client.event
+async def on_member_join(member):
+    for channel in member.guild.channels:
+        if str(channel) == "general":
+            await channel.send_message(f"""Welcome to the server {member.mention}""") 
         
 client.run(TOKEN)
